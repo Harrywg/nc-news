698aa67a920c6a7364c04795ef60b3dae2d2ca34
Harry Ward-Gray <wardgray24@gmail.com>
2023-08-15 14:23:14 +0100

    potentially fixed error

--- controllers/articles-controller.js
+++ controllers/articles-controller.js
@@ -4,6 +4,7 @@ exports.getArticlesById = (req, res, next) => {
   const { article_id } = req.params;
   selectArticles(article_id)
     .then((article) => {
+      console.log("123");
       res.status(200).send({ article });
     })
     .catch((err) => next(err));
--- error-handler.js
+++ error-handler.js
@@ -1,5 +1,4 @@
 const express = require("express");
 exports.errorHandler = (err, req, res, next) => {
-  console.log(err);
   res.status(500).send("Server Error");
 };
