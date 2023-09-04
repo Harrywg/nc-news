511a285d5c0c2f3156514759e86dd45f77836119
Harry Ward-Gray <wardgray24@gmail.com>
2023-09-04 09:42:56 +0100

    add cors

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const apiRouter = require("./routes/api-router");
 const { errorHandler } = require("./error-handler");
 
+app.use(cors());
 app.use(express.json());
 app.use("/api", apiRouter);
 app.use(errorHandler);
